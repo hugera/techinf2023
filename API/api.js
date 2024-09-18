93acc5cf2aac78e8f010704f6f65b01a79f9d3c1
Hugo Leonardo <hugao.leo123@gmail.com>
2024-09-18 10:31:11 -0300

    3°Atualizaçao 18/09/24

--- API/api.js
+++ API/api.js
@@ -1,13 +1,60 @@
-function converterparaJson(data){
-    const obj = data.json();
-    return obj;
+function getJsonData(respostaAPI){
+    const json = respostaAPI.json();
+    return json;    
 }
 
-function imprimirTabela(json){
-    console.table(json.cards, ["code", "image", "value", "suit"]);
-    console.log("restam", json.remaining, "cartas")
-}
+function atualizaInfo(jsonData){
+    if (!jsonData.success)
+    throw new Error("Erro ao acessar os dados da API");
+    
+    const deck_id = jsonData.deck_id;
+    const remaining = jsonData.remaining;
+    
+    const info1 = document.getElementById("deck_id");
+    const info2 = document.getElementById("remaining");   
+    
+    info1.innerText = deck_id;
+    info2.innerText = remaining;
 
-fetch(`https://deckofcardsapi.com/api/deck/dpizo53h8k8o/draw/?count=3`)
-.then(converterparaJson)
-.then(imprimirTabela);
+    return jsonData;
+} 
+
+function Reembaralhar(){
+    fetch("https://deckofcardsapi.com/api/deck/8acrl9s6p501/shuffle/?remaining=false")
+    .then(getJsonData)
+    .then(function(jsonData){
+        console.log = (jsonData);
+        if(jsonData.success){
+            alert("Baralho Reembaralhado")
+        }      
+    
+    })
+}         
+
+function puxarCartas(jsonData){
+        const cards = jsonData.cards;
+
+        for(let i=0; i<3; i++){
+        const id = `carta${i+1}`;
+        
+        const card = document.getElementById(id);
+        let img = card.querySelector('img')
+        
+        if(img == null){
+            img = document.createElement('img');      
+        }
+        img.setAttribute('src', cards[i].image);
+        card.append(img);
+
+        }
+
+     }    
+
+function olharCartas(){
+
+    fetch("https://deckofcardsapi.com/api/deck/8acrl9s6p501/draw/?count=3")
+    .then(getJsonData)
+    .then(atualizaInfo)
+    .then(puxarCartas)
+
+} 
