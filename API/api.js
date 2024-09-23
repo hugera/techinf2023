f77794c879ab4ea2556c86acda754b6994700713
Hugo Leonardo <hugao.leo123@gmailcom>
2024-09-23 09:56:17 -0300

    Trasnfomar o DEck ID em Privado

--- API/api.js
+++ API/api.js
@@ -2,94 +2,88 @@ let deckID = "6efwkrv87lp2";
 
 const deck = new Deck(deckID);
 
-function Deck(deckID){
-    this.deckID =deckID;
-    this.listofcards = "4C,7H,AS,7D,3C,3H,3S,3D,2C,2H,2S,2D,AC,AH,AD,KC,KH,KS,KD,QC,QH,QS,QD,JC,JH,JS,JD";
-
-this.olharCartas = function(){
-     fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=3`)
-    .then(getJsonData)
-    .then(atualizaInfo)
-    .then(puxarCartas)
-}
-
-this.Reembaralhar = function(){
-    fetch(`https://deckofcardsapi.com/api/deck/${deckID}/shuffle/?remaining=false`)
-    .then(getJsonData)
-    .then(atualizaInfo)
-    .then(function(jsonData){
-        console.log = (jsonData);
-        if(jsonData.success){
-            alert("Baralho Reembaralhado")
-        }      
-    
-    })
-
-}
-}
-
-
-// this.novoDeck = function(){
-//     fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?cards=4C,7H,AS,7D,3C,3H,3S,3D,2C,2H,2S,2D,AC,AH,AD,KC,KH,KS,KD,QC,QH,QS,QD,JC,JH,JS,JD")
-//     .then(getJsonData)
-//     .then(function(jsonData){
-//         deckID = jsonData.deck_id;
-//     })
-// }
-
-
-function getJsonData(respostaAPI){
-    const json = respostaAPI.json();
-    return json;    
-}
-
-function atualizaInfo(jsonData){
-    if (!jsonData.success)
-    throw new Error("Erro ao acessar os dados da API");
-    
-    const deck_id = jsonData.deck_id;
-    const remaining = jsonData.remaining;
-    
-    const info1 = document.getElementById("deck_id");
-    const info2 = document.getElementById("remaining");   
-    
-    info1.innerText = deck_id;
-    info2.innerText = remaining;
-
-    return jsonData;
-} 
-
-// function Reembaralhar(){
-//     fetch(`https://deckofcardsapi.com/api/deck/${deckID}/shuffle/?remaining=false`)
-//     .then(getJsonData)
-//     .then(atualizaInfo)
-//     .then(function(jsonData){
-//         console.log = (jsonData);
-//         if(jsonData.success){
-//             alert("Baralho Reembaralhado")
-//         }      
-    
-//     })
-// }         
-
-function puxarCartas(jsonData){
-        const cards = jsonData.cards;
-
-        for(let i=0; i<3; i++){
-        const id = `carta${i+1}`;
+    function Deck(deckID){
+        this.deckID =deckID;
+        this.listofcards = "4C,7H,AS,7D,3C,3H,3S,3D,2C,2H,2S,2D,AC,AH,AD,KC,KH,KS,KD,QC,QH,QS,QD,JC,JH,JS,JD";
         
-        const card = document.getElementById(id);
-        let img = card.querySelector('img')
-        
-        if(img == null){
-            img = document.createElement('img');      
-        }
-        img.setAttribute('src', cards[i].image);
-        card.append(img);
-
+        const setDeckId = (jsonData) => {
+            this.deckID = jsonData.deck_id
+            return jsonData;
         }
 
-     }    
+
+    this.olharCartas = function(){
+        fetch(`https://deckofcardsapi.com/api/deck/${this.deckID}/draw/?count=3`)
+        .then(getJsonData)
+        .then(atualizaInfo)
+        .then(puxarCartas)
+    }
+
+    this.Reembaralhar = function(){
+        fetch(`https://deckofcardsapi.com/api/deck/${this.deckID}/shuffle/?remaining=false`)
+        .then(getJsonData)
+        .then(atualizaInfo)
+        .then(function(jsonData){
+            console.log = (jsonData);
+            if(jsonData.success){
+                alert("Baralho Reembaralhado")
+            }      
+        
+        })
+
+    }
+    this.novoDeck = function(){
+        fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?cards=${this.listofcards}`)
+        .then(getJsonData)
+        .then(atualizaInfo)
+        .then(setDeckId)
+
+    }
+
+
+    }
+
+//Funções Privadas
+
+    function getJsonData(respostaAPI){
+        const json = respostaAPI.json();
+        return json;    
+    }
+
+    function atualizaInfo(jsonData){
+        if (!jsonData.success)
+        throw new Error("Erro ao acessar os dados da API");
+        
+        const deck_id = jsonData.deck_id;
+        const remaining = jsonData.remaining;
+        
+        const info1 = document.getElementById("deck_id");
+        const info2 = document.getElementById("remaining");   
+        
+        info1.innerText = deck_id;
+        info2.innerText = remaining;
+
+        return jsonData;
+    } 
+        
+
+    function puxarCartas(jsonData){
+            const cards = jsonData.cards;
+
+            for(let i=0; i<3; i++){
+            const id = `carta${i+1}`;
+            
+            const card = document.getElementById(id);
+            let img = card.querySelector('img')
+            
+            if(img == null){
+                img = document.createElement('img');      
+            }
+            img.setAttribute('src', cards[i].image);
+            card.append(img);
+
+            }
+    }    
 
 
 
